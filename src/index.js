72efb71e167b6e02c010a804b138b9b48b163b81
Leo <leoli_082000@hotmail.com>
2026-07-25 02:27:33 +0800

    Export MCP stdio helpers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export * from "./auth.js";
 export * from "./estimate.js";
 export * from "./git.js";
 export * from "./github.js";
+export * from "./mcp.js";
 export * from "./pricing.js";
 export * from "./server.js";
 export * from "./store.js";
