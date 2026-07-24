6e144b9af62753667feb9caba1c0a7d69064607c
Leo <leoli_082000@hotmail.com>
2026-07-25 01:56:29 +0800

    Export MCP server helpers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export * from "./estimate.js";
 export * from "./git.js";
 export * from "./github.js";
+export * from "./mcp.js";
 export * from "./pricing.js";
 export * from "./server.js";
 export * from "./store.js";
